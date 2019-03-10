94b1065bcec94f9c04717cc29f6b29cabd827f5c
shubhamdangare <shubham17495@gmail.com>
2019-03-10 11:50:24 +0530

    added unit test cases

--- dbservice/src/test/scala/com/example/UserDataDBServiceSpec.scala
+++ dbservice/src/test/scala/com/example/UserDataDBServiceSpec.scala
@@ -1,31 +1,49 @@
 package com.example
 
-import scalikejdbc._
+import java.sql.Timestamp
+import java.util.Calendar
+
+import org.specs2.mutable._
 import scalikejdbc.specs2.mutable.AutoRollback
-import org.specs2.mutable.Specification
 
-object UserDataDBServiceSpec extends Specification {
+class UserDataDBServiceSpec extends Specification {
 
-  sequential
-
-  "Member should create a new record" in new AutoRollback {
-    //val before = Member.count()
-    //Member.create(3, "Chris")
-   // Member.count() must_==(before + 1)
+  val userDataDBService = new UserDataDBService;
+  "count all records" in new AutoRollback {
+    val count = userDataDBService.getNumberOfUser()
+    count should be_>(0L)
   }
 
-  "Member should ... " in new AutoRollbackWithFixture {
+  "get user data" in new AutoRollback {
+    val data = userDataDBService.getUserData(3)
+    data.isDefined should beTrue
+  }
+  "check room booked already" in new AutoRollback {
+    val data = userDataDBService.checkBookingDoneAlready(303)
+    data.isDefined should beTrue
+  }
+  "check is booking is not empty" in new AutoRollback {
+    val data = userDataDBService.findAll()
+    data.isEmpty should beFalse
+  }
+  "check is booked room is deallocated" in new AutoRollback {
+    val data = userDataDBService.destroy(301)
+    assert(data == 0)
+  }
+  "covert time stamp" in new AutoRollback {
+    val calendar = Calendar.getInstance
+    val token_gen = new Timestamp(calendar.getTime.getTime)
+    val data = userDataDBService.getCalendarTimeStamp(token_gen.toString)
+    assert(data == token_gen)
+  }
 
+  "room booking for limited time" in new AutoRollback {
+    val data = userDataDBService.roomBooking("6", 3, 303)
+    assert(data != "maximum time allocated cannot book for that long")
+  }
+  "user not registered" in new AutoRollback {
+    val data = userDataDBService.roomBooking("1", 10, 303)
+    assert(data == "user not found")
   }
 
 }
-
-trait AutoRollbackWithFixture extends AutoRollback {
-  // override def db = NamedDB('db2).toDB
-  override def fixture(implicit session: DBSession) {
-    sql"insert into members values (1, ${"Alice"}, current_timestamp)".update.apply()
-    sql"insert into members values (2, ${"Bob"}, current_timestamp)".update.apply()
-  }
-}
-
-
